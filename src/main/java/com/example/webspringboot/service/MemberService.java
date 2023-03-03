3086da9c32200c079f0ca80bc60dfc30c78c2d8e
Sungwon Lee <sstxz444@gmail.com>
2023-03-03 11:57:35 +0900

    MemberController 제작
    MemberController 제작

--- src/main/java/com/example/webspringboot/service/MemberService.java
+++ src/main/java/com/example/webspringboot/service/MemberService.java
@@ -3,14 +3,18 @@ package com.example.webspringboot.service;
 import com.example.webspringboot.domain.Member;
 import com.example.webspringboot.repository.MemberRepository;
 import com.example.webspringboot.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
