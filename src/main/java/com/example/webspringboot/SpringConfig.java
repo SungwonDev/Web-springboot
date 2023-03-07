56c7471b55fbec28179766f84356678555dd3acc
Sungwon Lee <sstxz444@gmail.com>
2023-03-07 17:13:00 +0900

    DB 여러기능으로 만들어보기
    JPA 사용해보기

---
+++ src/main/java/com/example/webspringboot/SpringConfig.java
@@ -0,0 +1,35 @@
+package com.example.webspringboot;
+
+import com.example.webspringboot.repository.JpaMemberRepository;
+import com.example.webspringboot.repository.MemberRepository;
+import com.example.webspringboot.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+import javax.swing.*;
+
+@Configuration
+public class SpringConfig {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+
+
+//    @Bean
+//    public MemberRepository memberRepository(){
+//
+//        return new JpaMemberRepository(em);
+//    }
+}
